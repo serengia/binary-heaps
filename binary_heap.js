3951819f6e1b50d9b38394eb170e80e96d201f83
James Serengia <jamesserengia@gmail.com>
2023-12-18 13:34:06 +0300

    Start implementation of extract max

--- binary_heap.js
+++ binary_heap.js
@@ -17,10 +17,30 @@ class MaxBinaryHeap {
         this.values[parentIndex] = ele;
         this.values[index] = parent;
         index = parentIndex;
-      }
+      } else break;
     }
 
     return this.values;
+  }
+
+  extractMax() {
+    const max = this.values[0];
+    const last = this.values.pop();
+    this.values[0] = last;
+
+    let index = 0;
+
+    while (index < this.values.length) {
+      let currentParent = this.values[index];
+      let leftChild = this.values[2(index + 1)] || 0;
+      let rightChild = this.values[2(index + 2)] || 0;
+
+      if (currentParent < Math.max(leftChild, rightChild)) {
+        // swap
+      }
+    }
+
+    return max;
   }
 }
 
