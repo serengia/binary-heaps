cd2c8c57da8f786eb8fe7534d9fa241dfa262fbe
James Serengia <jamesserengia@gmail.com>
2023-12-28 20:42:28 +0300

    Add dequeue method

--- binary_heap.js
+++ binary_heap.js
@@ -23,9 +23,45 @@ class MaxBinaryHeap {
     return this.values;
   }
 
-  extractMax() {
-    const max = this.values[0];
-    const last = this.values.pop();
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
   }
 }
 
@@ -34,11 +70,11 @@ const initialValues = [41, 39, 33, 18, 27, 12];
 const heap = new MaxBinaryHeap(initialValues);
 // console.log(heap.insert(55));
 console.log(heap.values);
-console.log(heap.extractMax());
+console.log(heap.dequeue());
 console.log(heap.values);
 
-console.log(heap.extractMax());
+console.log(heap.dequeue());
 console.log(heap.values);
 
-console.log(heap.extractMax());
+console.log(heap.dequeue());
 console.log(heap.values);
